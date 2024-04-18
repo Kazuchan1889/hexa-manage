c9d93d59f74311dff38958062dde3d34c474bb47
kazuchan1889 <you@example.com>
2024-04-18 13:57:06 +0700

    commit

--- src/page/DashboardUser.jsx
+++ src/page/DashboardUser.jsx
@@ -81,16 +81,16 @@ function DashboardUser() {
       style={{ backgroundColor: "#F0F0F0" }}
     >
       <NavbarUser />
-      <div className="flex h-fit">
+      <div className="flex h-fit " style={{ backgroundColor: "#F0F0F0" }}>
         <div className="h-full w-[95%] flex flex-col justify-center items-center mx-auto">
           <div className="h-[40rem] lg:h-full w-full flex flex-col lg:flex-row justify-between items center">
             <div className="flex justify-between items-center drop-shadow-lg bg-home px-5 lg:px-10 py-10 my-5 rounded-md w-[100%] lg:w-[100%]">
               <ProfileDashboard />
             </div>
           </div>
-          <div className="mb-12 flex flex-col lg:flex-row justify-between items-center w-full h-fit lg:h-1/2">
+          <div className="mb-12 mr-100 flex flex-col lg:flex-row justify-between items-center w-full h-fit lg:h-1/2">
             {!isMobile && (
-              <div className=" grid grid-cols-4 gap-6">
+              <div className=" grid grid-cols-4 gap-10">
                 <div className="flex items-center justify-center h-full my-8w-[30%] h-[23rem] lg:m-0 drop-shadow-lg bg-white p-10 rounded-xl border">
                   <ChartDataKehadiranUser />
                 </div>
@@ -106,12 +106,11 @@ function DashboardUser() {
               </div>
             )}
           </div>
-            <div className=" w-[100%] lg:w-[69.5%] h-[39rem] lg:h-[23rem] lg:m-0 drop-shadow-lg bg-white p-10 rounded-xl border">
-            <LaporanKegiatanDashboard />
+          <div className="w-[100%] lg:w-[69.5%] h-[39rem] lg:h-[23rem] lg:mr-100 ml-auto mb-4 drop-shadow-lg bg-white p-10 rounded-xl border">
+
+              <LaporanKegiatanDashboard />
             </div>
-           <div className="h-[40rem]lg:h-[17rem] w-full flex flex-col lg:flex-row justify-between items center">
-            
-          </div>
+
         </div>
       </div>
     </div>
