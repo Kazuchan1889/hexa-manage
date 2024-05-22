bc68eb3c5944baed81ded45d73841334087a893b
Jans <janshenfu.y.t@gmail.com>
2024-05-22 14:34:30 +0700

    commit

--- src/minicomponent/Shortcut.jsx
+++ src/minicomponent/Shortcut.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 function Shortcut() {
     return (
-        <div className="mt-6">
+        <div className="mt-6 mb-2">
             <h1 className="text-sm">Shortcut</h1>
             <div className="flex flex-row mt-4 gap-6">
-                <div className="flex border border-black rounded-full px-4 py-2 text-center hover:border-white">
+                <div className="flex border border-white rounded-full px-4 py-2 text-center hover:border-black">
                     <a className="text-sm" href="/LiveAttendance">Live Attendance</a>
                 </div>
                 <div className="flex border border-white rounded-full px-4 py-2 text-center hover:border-black">
--- src/page/Formovertime.jsx
+++ src/page/Formovertime.jsx
@@ -6,7 +6,7 @@ import ip from "../ip";
 function Formover() {
     const [formData, setFormData] = useState({
         note: "",
-        tipe: "", 
+        tipe: "",
         mulai: "",
         selesai: "",
         breaktime: "",
@@ -41,17 +41,17 @@ function Formover() {
             setSubmitStatus('error'); // Atur status pengiriman menjadi error
         }
     };
-    
+
     const handleFileChange = (e) => {
         const file = e.target.files[0]; // Mendapatkan file yang diunggah
         const reader = new FileReader(); // Membuat objek FileReader
-    
+
         // Mengatur fungsi onload untuk menjalankan setelah file selesai dibaca
         reader.onload = () => {
             const base64Data = reader.result; // Mendapatkan data URL (base64) dari file
             setFile(base64Data); // Menyimpan data URL ke state
         };
-    
+
         // Membaca file sebagai data URL
         reader.readAsDataURL(file);
     };
@@ -78,47 +78,48 @@ function Formover() {
                     Terjadi kesalahan saat mengirim overtime. Silakan coba lagi.
                 </div>
             )}
-            <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-                Request Overtime
-            </h2>
-            <div className="mt-24 grid grid-rows-6 gap-x-6 gap-y-8">
+            <div className="my-4 flex justify-center ">
                 <form onSubmit={handleSubmit}>
-                    <div className="mb-5">
-                        <label htmlFor="note" className='block text-sm font-medium leading-6 text-gray-900 mr-60'>Catatan:</label>
-                        <input type="text" id="note" name="note" value={formData.note} onChange={handleInputChange}
-                            className=" w-80 rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
-                    </div>
-                    <div className="mb-5">
-                          <label htmlFor="overtimeType" className='block text-sm font-medium leading-6 text-gray-900 mr-60'>Jenis Overtime:</label>
+                    <div className='w-full border border-black p-8 rounded-3xl'>
+                        <h2 className="text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+                            Request Overtime
+                        </h2>
+                        <div className="mb-2">
+                            <label htmlFor="note" className='block text-sm font-medium leading-6 text-gray-900 text-left mb-2'>Catatan:</label>
+                            <input type="text" id="note" name="note" value={formData.note} onChange={handleInputChange}
+                                className=" w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                        </div>
+                        <div className="mb-2">
+                            <label htmlFor="overtimeType" className='block text-sm font-medium leading-6 text-gray-900 text-left mb-2'>Jenis Overtime:</label>
                             <select id="overtimeType" name="tipe" value={formData.tipe} onChange={handleInputChange}
-                                        className="w-80 rounded-md border-0 py-1.5 pl-3 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                                className="w-full rounded-md border-0 py-1.5 pl-3 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
                                 <option value="">Pilih Jenis Overtime</option>
                                 <option value="Sesudah Shift">Sesudah Shift</option>
                                 <option value="Sebelum Shift">Sebelum Shift</option>
-                        </select>
+                            </select>
+                        </div>
+                        <div className="mb-2">
+                            <label htmlFor="startTime" className='block text-sm font-medium leading-6 text-gray-900 text-left mb-2'>Dari Jam:</label>
+                            <input type="time" id="mulai" name="mulai" value={formData.mulai} onChange={handleInputChange}
+                                className=" w-[36%] rounded-md border-0 py-1.5 px-4 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                            <span className='mx-4'> sampai </span>
+                            <input type="time" id="selesai" name="selesai" value={formData.selesai} onChange={handleInputChange}
+                                className=" w-[36%] rounded-md border-0 py-1.5 px-4 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                        </div>
+                        <div className="mb-2">
+                            <label htmlFor="breakTime" className='block text-sm font-medium leading-6 text-gray-900 text-left mb-2'>Istirahat (menit):</label>
+                            <input type="number" id="breakTime" name="breakTime" value={formData.breakTime} onChange={handleInputChange}
+                                className=" w-full rounded-md border-0 py-1.5 px-4 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                        </div>
+                        <div className="mb-5">
+                            <label htmlFor="file" className='block text-sm font-medium leading-6 text-gray-900 text-left mb-2'>Upload Foto</label>
+                            <input type="file" id="file" name="file" onChange={handleFileChange}
+                                className="w-full text-gray-900" />
+                        </div>
+                        <button type="submit" className="w-full rounded-lg bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                            Submit
+                        </button>
                     </div>
-
-                    <div className="mb-5">
-                        <label htmlFor="startTime" className='block text-sm font-medium leading-6 text-gray-900 mr-60'>Dari Jam:</label>
-                        <input type="time" id="mulai" name="mulai" value={formData.mulai} onChange={handleInputChange}
-                            className=" w-1/5 rounded-md border-0 py-1.5 px-4 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
-                        <span> sampai </span>
-                        <input type="time" id="selesai" name="selesai" value={formData.selesai} onChange={handleInputChange}
-                            className=" w-1/5 rounded-md border-0 py-1.5 px-4 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
-                    </div>
-                    <div className="mb-5">
-                        <label htmlFor="breakTime" className='block text-sm font-medium leading-6 text-gray-900 mr-60'>Istirahat (menit):</label>
-                        <input type="number" id="breakTime" name="breakTime" value={formData.breakTime} onChange={handleInputChange}
-                            className=" w-1/5 rounded-md border-0 py-1.5 px-4 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
-                    </div>
-                    <div className="mb-5">
-                        <label htmlFor="file" className='block text-sm font-medium leading-6 text-gray-900 mr-60'>Upload Foto:</label>
-                        <input type="file" id="file" name="file" onChange={handleFileChange}
-                            className=" w-80 text-gray-900" />
-                    </div>
-                    <button type="submit" className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
-                        Submit
-                    </button>
                 </form>
             </div>
         </div>
