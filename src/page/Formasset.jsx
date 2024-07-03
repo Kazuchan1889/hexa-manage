83e36cfd4f69b63ebab58e3d0b8523900543797f
Jans <janshenfu.y.t@gmail.com>
2024-07-03 16:35:22 +0700

    commit

--- src/page/Formasset.jsx
+++ src/page/Formasset.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import ip from "../ip";
-
+import CloseIcon from "@mui/icons-material/Close";
 const AssetForm = () => {
   const [formData, setFormData] = useState({
     nama_barang: '',
@@ -36,7 +36,7 @@ const AssetForm = () => {
 
       // Tampilkan notifikasi
       alert("Data berhasil ditambahkan!");
-      
+
       // Reset form
       setFormData({
         nama_barang: '',
@@ -54,31 +54,40 @@ const AssetForm = () => {
   };
 
   return (
-    <div className="max-w-md mx-auto">
-      <h2 className="text-lg font-semibold mb-4">Form Input Data Barang</h2>
-      <form onSubmit={handleSubmit}>
-        <div className="mb-4">
-          <label htmlFor="nama_barang" className="block mb-1">Nama Barang:</label>
-          <input type="text" id="nama_barang" name="nama_barang" value={formData.nama_barang} onChange={handleChange} className="w-full px-3 py-2 border rounded-md" />
-        </div>
-        <div className="mb-4">
-          <label htmlFor="merek" className="block mb-1">Merek:</label>
-          <input type="text" id="merek" name="merek" value={formData.merek} onChange={handleChange} className="w-full px-3 py-2 border rounded-md" />
-        </div>
-        <div className="mb-4">
-          <label htmlFor="model_tipe" className="block mb-1">Model/Tipe:</label>
-          <input type="text" id="model_tipe" name="model_tipe" value={formData.model_tipe} onChange={handleChange} className="w-full px-3 py-2 border rounded-md" />
-        </div>
-        <div className="mb-4">
-          <label htmlFor="harga" className="block mb-1">Harga:</label>
-          <input type="number" id="harga" name="harga" value={formData.harga} onChange={handleChange} className="w-full px-3 py-2 border rounded-md" />
-        </div>
-        <div className="mb-4">
-          <label htmlFor="jumlah" className="block mb-1">Jumlah:</label>
-          <input type="number" id="jumlah" name="jumlah" value={formData.jumlah} onChange={handleChange} className="w-full px-3 py-2 border rounded-md" />
-        </div>
-        <button type="submit" className="bg-blue-500 text-white px-4 py-2 rounded-md">Submit</button>
-      </form>
+    <div className="absolute top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-50 z-50">
+      <div className="bg-white rounded-3xl flex justify-center overflow-hidden">
+        <form onSubmit={handleSubmit}>
+          <div className='w-[25rem] border p-8 h-[25rem] rounded-3xl overflow-scroll'>
+            <div className='flex justify-center mb-4'>
+              <h2 className="text-center text-lg font-semibold m-auto">Item Data Input Form</h2>
+              <button  className="focus:outline-none">
+                <CloseIcon />
+              </button>
+            </div>
+            <div className="mb-4">
+              <label htmlFor="nama_barang" className="block mb-1 text-left">Item Name</label>
+              <input type="text" id="nama_barang" name="nama_barang" value={formData.nama_barang} onChange={handleChange} className="w-full px-3 py-2 border rounded-md" />
+            </div>
+            <div className="mb-4">
+              <label htmlFor="merek" className="block mb-1 text-left">Brand</label>
+              <input type="text" id="merek" name="merek" value={formData.merek} onChange={handleChange} className="w-full px-3 py-2 border rounded-md" />
+            </div>
+            <div className="mb-4">
+              <label htmlFor="model_tipe" className="block mb-1 text-left">Tipe</label>
+              <input type="text" id="model_tipe" name="model_tipe" value={formData.model_tipe} onChange={handleChange} className="w-full px-3 py-2 border rounded-md" />
+            </div>
+            <div className="mb-4">
+              <label htmlFor="harga" className="block mb-1 text-left">Price</label>
+              <input type="number" id="harga" name="harga" value={formData.harga} onChange={handleChange} className="w-full px-3 py-2 border rounded-md" />
+            </div>
+            <div className="mb-4">
+              <label htmlFor="jumlah" className="block mb-1 text-left">Quantity</label>
+              <input type="number" id="jumlah" name="jumlah" value={formData.jumlah} onChange={handleChange} className="w-full px-3 py-2 border rounded-md" />
+            </div>
+            <button type="submit" className="w-full bg-blue-500 text-white px-4 py-2 rounded-md">Submit</button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
