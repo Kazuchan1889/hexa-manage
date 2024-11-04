851c0880221b57dd06e0fed97fbbd05a6254ffd9
kazuchan1889 <you@example.com>
2024-11-04 16:12:22 +0700

    commit

--- src/ip.jsx
+++ src/ip.jsx
@@ -1,6 +1,8 @@
-       const ip = "http://localhost:3000";
-    //  const ip = "https://fb5e-182-3-44-19.ngrok-free.app";
-    //  const ip = "http://192.168.137.25:3000";
+        const ip = "http://localhost:3000";
+    // const ip = "http://10.42.0.1:3000"
+    // const ip = "https://26ce-2a09-bac5-39f4-155f-00-221-5.ngrok-fr"
+    //   const ip = "https://3ec2-117-54-99-210.ngrok-free.app";
+    //   const ip = "http://192.168.1.27:3000";
         //    const ip = "https://hbm.hris.com";
 
 export default ip;
