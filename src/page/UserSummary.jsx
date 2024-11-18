e88a07635a3179ee0c0fe270f2d342039e3ff9ab
Jans <janshenfu.y.t@gmail.com>
2024-11-18 10:27:43 +0700

    commit

---
+++ src/page/UserSummary.jsx
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from 'react';
+import NavbarUser from '../feature/NavbarUser';
+const UserSummary = () => {
+
+    return (
+        <div style={{ backgroundColor: "#F0F0F0" }}>
+            <NavbarUser />
+            <div>
+                dfd
+            </div>
+        </div>
+    );
+}
+
+export default UserSummary;
