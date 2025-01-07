7e04255356280581905aea0585895aaf90b62b39
Jans <janshenfu.y.t@gmail.com>
2025-01-07 13:26:04 +0700

    commit

--- src/minicomponent/ViewAnnounce.jsx
+++ src/minicomponent/ViewAnnounce.jsx
@@ -1,10 +1,10 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import ip from "../ip";
-import AnnouncementEdit from './AnnouncementEdit';
-import { useDispatch, useSelector } from "react-redux"; // import dispatch and useSelector
-import { loadingAction } from "../store/store"; // import loading action
-import Loading from "../page/Loading"; // import Loading component
+import AnnouncementEdit from "./AnnouncementEdit";
+import { useDispatch, useSelector } from "react-redux";
+import { loadingAction } from "../store/store";
+import Loading from "../page/Loading";
 
 const AnnouncementList = () => {
   const [announcements, setAnnouncements] = useState([]);
@@ -12,68 +12,67 @@ const AnnouncementList = () => {
   const apiUrl = `${ip}/api/announcment`;
   const isUserAdmin = localStorage.getItem("role");
 
-  // Dispatch and loading state
+  // Redux dispatch and loading state
   const dispatch = useDispatch();
-  const loading = useSelector((state) => state.loading.isLoading); // Get loading state from redux
+  const loading = useSelector((state) => state.loading.isLoading);
 
   useEffect(() => {
     fetchAnnouncements();
   }, []);
 
   const fetchAnnouncements = async () => {
-    dispatch(loadingAction.startLoading(true)); // Start loading
+    dispatch(loadingAction.startLoading(true));
     try {
-      const accessToken = localStorage.getItem('accessToken');
+      const accessToken = localStorage.getItem("accessToken");
       const response = await axios.get(`${apiUrl}/get`, {
         headers: {
-          'Authorization': accessToken,
-          'Content-Type': 'application/json'
-        }
+          Authorization: accessToken,
+          "Content-Type": "application/json",
+        },
       });
       setAnnouncements(response.data);
     } catch (error) {
-      console.error('Error fetching announcements:', error);
+      console.error("Error fetching announcements:", error);
     } finally {
-      dispatch(loadingAction.startLoading(false)); // Stop loading
+      dispatch(loadingAction.startLoading(false));
     }
   };
 
   const formatDate = (dateString) => {
     const date = new Date(dateString);
     const day = date.getDate();
-    const month = date.getMonth() + 1; // Months are zero-based
+    const month = date.getMonth() + 1;
     const year = date.getFullYear();
-    return `${day < 10 ? '0' + day : day}-${month < 10 ? '0' + month : month}-${year}`;
+    return `${day < 10 ? "0" + day : day}-${month < 10 ? "0" + month : month}-${year}`;
   };
 
   const handleDelete = async (id) => {
     try {
-      const accessToken = localStorage.getItem('accessToken');
+      const accessToken = localStorage.getItem("accessToken");
       await axios.delete(`${apiUrl}/delete/${id}`, {
         headers: {
-          'Authorization': accessToken,
-          'Content-Type': 'application/json'
-        }
+          Authorization: accessToken,
+          "Content-Type": "application/json",
+        },
       });
-      console.log('Announcement deleted successfully');
-      fetchAnnouncements(); // Refresh the list after deletion
+      fetchAnnouncements();
     } catch (error) {
-      console.error('Error deleting announcement:', error);
+      console.error("Error deleting announcement:", error);
     }
   };
 
   const renderDownloadButton = (attachment) => {
-    if (attachment && attachment.trim() !== '') {
+    if (attachment && attachment.trim() !== "") {
       return (
         <a
           href={attachment}
           download
-          className="flex items-center justify-center bg-white border border-green-500 px-4 py-2 mt-2 rounded text-green-500"
+          className="flex items-center justify-center bg-white border border-green-500 px-3 py-1 mt-2 rounded text-green-500 text-xs lg:text-sm"
           title="Download Attachment"
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            className="h-5 w-5"
+            className="h-4 w-4 lg:h-5 lg:w-5"
             viewBox="0 0 20 20"
             fill="currentColor"
           >
@@ -94,41 +93,49 @@ const AnnouncementList = () => {
     return null;
   };
 
-  // Render loading component when loading is true
   if (loading) {
     return <Loading />;
   }
 
   return (
     <div className="container mx-auto p-4 rounded-lg">
-      <div className="sticky top-0 bg-white z-10">
-        <h1 className="text-xl font-bold mb-4">Assignment</h1>
+      <div className="sticky top-0 bg-white z-10 shadow-md p-2 mb-4">
+        <h1 className="text-lg lg:text-xl font-bold">Assignment</h1>
       </div>
-      <div id="announcement-list" className="h-[12rem] mb-2 overflow-scroll">
+      <div
+        id="announcement-list"
+        className="h-[12rem] max-h-[20rem] overflow-auto space-y-4"
+      >
         {announcements.length > 0 ? (
           announcements.map((announcement, index) => (
             <div
               key={index}
-              className="flex items-start bg-white p-4 rounded-xl border border-gray-400 mb-4 shadow"
+              className="flex flex-col lg:flex-row items-start bg-white p-4 rounded-xl border border-gray-300 shadow-md space-y-2 lg:space-y-0"
             >
-              <div className="flex-1 pr-4">
-                <h2 className="text-2xl font-semibold">{announcement.title}</h2>
-                <p className="text-left">{announcement.description}</p>
+              <div className="flex-1 pr-0 lg:pr-4">
+                <h2 className="text-sm lg:text-lg font-semibold">
+                  {announcement.title}
+                </h2>
+                <p className="text-xs lg:text-sm text-gray-600">
+                  {announcement.description}
+                </p>
               </div>
-              <div className="flex flex-col items-end justify-between ml-auto">
-                <p>{formatDate(announcement.tanggal_upload)}</p>
+              <div className="flex flex-col items-start lg:items-end space-y-2">
+                <p className="text-xs lg:text-sm text-gray-500">
+                  {formatDate(announcement.tanggal_upload)}
+                </p>
                 <div className="flex space-x-2">
                   {renderDownloadButton(announcement.attachment)}
                   {isUserAdmin === "admin" && (
                     <>
                       <button
-                        className="bg-blue-500 text-white px-4 py-2 mt-2 rounded"
+                        className="bg-blue-500 text-white text-xs lg:text-sm px-3 py-1 rounded"
                         onClick={() => setEditAnnouncement(announcement)}
                       >
                         Edit
                       </button>
                       <button
-                        className="bg-red-500 text-white px-4 py-2 mt-2 rounded"
+                        className="bg-red-500 text-white text-xs lg:text-sm px-3 py-1 rounded"
                         onClick={() => handleDelete(announcement.id)}
                       >
                         Delete
@@ -140,7 +147,9 @@ const AnnouncementList = () => {
             </div>
           ))
         ) : (
-          <div className="text-center text-gray-500 mt-4">No assignments today</div>
+          <div className="text-center text-gray-500 text-xs lg:text-sm mt-4">
+            No assignments today
+          </div>
         )}
       </div>
       {editAnnouncement && (
@@ -152,5 +161,6 @@ const AnnouncementList = () => {
       )}
     </div>
   );
-}  
+};
+
 export default AnnouncementList;
