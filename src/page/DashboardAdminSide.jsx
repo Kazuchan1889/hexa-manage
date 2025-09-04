a867c0616a464659ce9748e1c8cc0a0ba50d1a3b
Kazuchan1889 <134587893+Kazuchan1889@users.noreply.github.com>
2025-09-04 13:32:42 +0700

    commit

--- src/page/DashboardAdminSide.jsx
+++ src/page/DashboardAdminSide.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { Button, IconButton } from "@mui/material";
 import Swal from "sweetalert2";
-import NavbarUser from "../feature/MobileNav";
+import NavbarUser from "../feature/NavbarUser";
 import ChartDataKaryawan from "../feature/ChartDataKaryawan";
 import ChartDataKehadiran from "../feature/ChartDataKehadiran";
 import ChartDataKehadiranUser from "../feature/ChartDataKehadiranUser";
@@ -12,11 +12,6 @@ import AnnouncementList from "../minicomponent/ViewAnnounce";
 import Announcment from "../minicomponent/Announcment";
 import ip from "../ip";
 import UserIcon from '@mui/icons-material/AccountCircle';
-import { useDispatch, useSelector } from "react-redux";
-import Loading from "../page/Loading";
-import CheckCircleIcon from '@mui/icons-material/CheckCircle';
-import ErrorIcon from '@mui/icons-material/Error';
-import WarningIcon from '@mui/icons-material/Warning';
 import Sidebar from "../feature/Sidebar";
 import Headb from "../feature/Headbar";
 import Shortcut from "../minicomponent/Shortcut";
@@ -48,14 +43,9 @@ function DashboardAdminSide() {
         jabatan: "",
         cutimandiri: ""
     });
-    const [loadingSchedule, setLoadingSchedule] = useState(false);
-    const [loadingAbsensi, setLoadingAbsensi] = useState(false);
-    const [loadingAnnouncement, setLoadingAnnouncement] = useState(false);
 
     const navigate = useNavigate();
     const checkOperation = localStorage.getItem("operation");
-    const dispatch = useDispatch();
-    const loading = useSelector((state) => state.loading.isLoading);
 
     useEffect(() => {
         const handleResize = () => setIsMobile(window.innerWidth <= 1024);
@@ -87,7 +77,6 @@ function DashboardAdminSide() {
 
     useEffect(() => {
         const fetchScheduleItems = async () => {
-            setLoadingSchedule(true);
             try {
                 const id = getIdFromToken();
                 if (!id) {
@@ -100,13 +89,10 @@ function DashboardAdminSide() {
                 setScheduleItems(response.data);
             } catch (error) {
                 console.error("Error fetching schedule items:", error);
-            } finally {
-                setLoadingSchedule(false);
             }
         };
 
         const fetchAbsensiItems = async () => {
-            setLoadingAbsensi(true);
             try {
                 const response = await axios.post(
                     `${ip}/api/absensi/get/data/dated`,
@@ -116,14 +102,12 @@ function DashboardAdminSide() {
                 setAbsensiItems(response.data);
             } catch (error) {
                 console.error("Error fetching absensi items:", error);
-            } finally {
-                setLoadingAbsensi(false);
             }
         };
 
         fetchScheduleItems();
         fetchAbsensiItems();
-    }, [dispatch]);
+    }, []);
 
     const formatDate = (dateString) => {
         const options = { day: '2-digit', month: '2-digit', year: 'numeric' };
@@ -169,35 +153,33 @@ function DashboardAdminSide() {
                     <div className={`${isMobile ? 'w-full' : 'w-1/4'} drop-shadow-lg bg-white p-4 rounded-xl border h-[23rem]`}>
                         <span className="text-[#204682] text-lg text-center font-bold">Today's Absence</span>
                         <div className="max-h-[18rem] mt-2 overflow-y-auto">
-                            {loadingAbsensi ? <Loading /> : (
-                                <ul className="space-y-3">
-                                    {absensiItems.length === 0 ? (
-                                        <p className="text-gray-500 text-center">Tidak ada data absensi hari ini.</p>
-                                    ) : (
-                                        absensiItems.map((item, index) => (
-                                            <li key={index} className="flex items-center p-3 border rounded-lg">
-                                                {item.photo ? (
-                                                    <img src={item.photo.startsWith("data:image/") ? item.photo : `data:image/jpeg;base64,${item.photo}`}
-                                                        className="w-8 h-8 rounded-full object-cover" alt="Absensi Photo" />
-                                                ) : (
-                                                    <div className="w-8 h-8 flex items-center justify-center rounded-full bg-gray-300">
-                                                        <UserIcon style={{ fontSize: 30, color: 'black' }} />
-                                                    </div>
-                                                )}
-                                                <div className="flex flex-col mx-auto">
-                                                    <span className="text-sm lg:text-md font-semibold">{item.nama}</span>
-                                                    <span className="text-xs text-gray-600">{item.status}</span>
+                            <ul className="space-y-3">
+                                {absensiItems.length === 0 ? (
+                                    <p className="text-gray-500 text-center">Tidak ada data absensi hari ini.</p>
+                                ) : (
+                                    absensiItems.map((item, index) => (
+                                        <li key={index} className="flex items-center p-3 border rounded-lg">
+                                            {item.photo ? (
+                                                <img src={item.photo.startsWith("data:image/") ? item.photo : `data:image/jpeg;base64,${item.photo}`}
+                                                    className="w-8 h-8 rounded-full object-cover" alt="Absensi Photo" />
+                                            ) : (
+                                                <div className="w-8 h-8 flex items-center justify-center rounded-full bg-gray-300">
+                                                    <UserIcon style={{ fontSize: 30, color: 'black' }} />
                                                 </div>
-                                                <div className="my-auto">
-                                                    {item.status === 'tanpa alasan' && <ErrorIcon className="text-red-500" />}
-                                                    {item.status === 'terlambat' && <WarningIcon className="text-yellow-500" />}
-                                                    {['masuk', 'libur', 'izin', 'cuti'].includes(item.status) && <CheckCircleIcon className="text-green-500" />}
-                                                </div>
-                                            </li>
-                                        ))
-                                    )}
-                                </ul>
-                            )}
+                                            )}
+                                            <div className="flex flex-col mx-auto">
+                                                <span className="text-sm lg:text-md font-semibold">{item.nama}</span>
+                                                <span className="text-xs text-gray-600">{item.status}</span>
+                                            </div>
+                                            <div className="my-auto">
+                                                {item.status === 'tanpa alasan' && <ErrorIcon className="text-red-500" />}
+                                                {item.status === 'terlambat' && <WarningIcon className="text-yellow-500" />}
+                                                {['masuk', 'libur', 'izin', 'cuti'].includes(item.status) && <CheckCircleIcon className="text-green-500" />}
+                                            </div>
+                                        </li>
+                                    ))
+                                )}
+                            </ul>
                         </div>
                     </div>
 
@@ -210,37 +192,33 @@ function DashboardAdminSide() {
                     <div className={`${isMobile ? 'w-full' : 'w-1/4'} drop-shadow-lg bg-white p-4 rounded-xl border h-[23rem]`}>
                         <span className="text-[#204682] text-lg text-center font-bold">Upcoming Schedule</span>
                         <div className="max-h-[18rem] overflow-y-auto flex flex-col justify-center items-center">
-                            {loadingSchedule ? (
-                                <Loading />
-                            ) : (
-                                <ul className="space-y-4 mt-4 w-full">
-                                    {(() => {
-                                        const filteredSchedules = scheduleItems.filter((item) => {
-                                            const scheduleDate = new Date(item.tanggal_mulai);
-                                            const today = new Date();
-                                            today.setHours(0, 0, 0, 0);
-                                            return scheduleDate >= today;
-                                        });
+                            <ul className="space-y-4 mt-4 w-full">
+                                {(() => {
+                                    const filteredSchedules = scheduleItems.filter((item) => {
+                                        const scheduleDate = new Date(item.tanggal_mulai);
+                                        const today = new Date();
+                                        today.setHours(0, 0, 0, 0);
+                                        return scheduleDate >= today;
+                                    });
 
-                                        if (filteredSchedules.length === 0) {
-                                            return <li className="text-gray-500 text-center text-xs lg:text-sm w-full flex justify-center items-center h-full">There is no schedule</li>;
-                                        }
+                                    if (filteredSchedules.length === 0) {
+                                        return <li className="text-gray-500 text-center text-xs lg:text-sm w-full flex justify-center items-center h-full">There is no schedule</li>;
+                                    }
 
-                                        return filteredSchedules.map((item, index) => (
-                                            <li key={index} className="border p-4 rounded-lg shadow-sm">
-                                                <div className="text-lg font-semibold">{item.judul}</div>
-                                                <div className="text-sm text-gray-600">{formatDate(item.tanggal_mulai)}</div>
-                                                <div className="text-sm text-gray-600">{item.mulai}</div>
-                                                <div className="mt-4 p-2 bg-blue-500 rounded-lg">
-                                                    <button onClick={handleReadMore} className="text-white font-semibold px-4 py-2 rounded-md">
-                                                        Read More
-                                                    </button>
-                                                </div>
-                                            </li>
-                                        ));
-                                    })()}
-                                </ul>
-                            )}
+                                    return filteredSchedules.map((item, index) => (
+                                        <li key={index} className="border p-4 rounded-lg shadow-sm">
+                                            <div className="text-lg font-semibold">{item.judul}</div>
+                                            <div className="text-sm text-gray-600">{formatDate(item.tanggal_mulai)}</div>
+                                            <div className="text-sm text-gray-600">{item.mulai}</div>
+                                            <div className="mt-4 p-2 bg-blue-500 rounded-lg">
+                                                <button onClick={handleReadMore} className="text-white font-semibold px-4 py-2 rounded-md">
+                                                    Read More
+                                                </button>
+                                            </div>
+                                        </li>
+                                    ));
+                                })()}
+                            </ul>
                         </div>
                     </div>
                 </div>
